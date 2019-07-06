7a30c6b29262f4c1cbaaff357bb5f4fbc8e1973f
David Tschida <dtschida@Davids-MBP.fios-router.home>
2019-07-06 17:31:25 -0400

    Fixed Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-      <h1>Header Component</h1>
+      <h1>{props.siteTitle}</h1>
     </header>
   );
 };
